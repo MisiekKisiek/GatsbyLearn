03178150275e89c25c19552f5f6c57a176722c10
MisiekKisiek <skrzypkka@gmail.com>
2020-10-23 16:52:58 +0200

    Initial commit from gatsby: (https://github.com/gatsbyjs/gatsby-starter-hello-world.git)

---
+++ src/pages/about.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const About = () => {
+    return ( <>
+        <h1>Siemka, o mnie stronka</h1>
+        <h2>Ale jestem gość</h2>
+    </> );
+}
+ 
+export default About;
---
+++ src/pages/contact.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Contact = () => {
+    return ( <>
+        <h1>Contact</h1>
+        <p>You can find me on <a href="https://facebook.com">Facebook</a></p>
+    </> );
+}
+ 
+export default Contact;
